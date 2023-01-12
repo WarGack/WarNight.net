4cc0a2d7e24d449a93aa309f4f3c235f77178341
WarGack <53942687+WarGack@users.noreply.github.com>
2023-01-12 23:35:58 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,1 @@
-<script type="text/javascript">
-    function redirect() {
-    
-        // Массив со ссылками
-        var links = ['http://link1.ru', 'http://link2.ru','http://link3.ru', 'http://link4.ru'];
- 
-        // Получаем рандомный элемент...
-        var rand = Math.floor(Math.random() * links.length);
- 
-        // ...и перенаправляем
-        window.location = links[rand];
-    }
- 
-    setTimeout(redirect, 9000);
-</script>
+alert("Hello, World!");

3aefb2d2fd5bfafede5373fc19e6a36a1d7d62ec
WarGack <53942687+WarGack@users.noreply.github.com>
2024-04-28 23:58:54 +0300

    Update testbd.js

--- functions/testbd.js
+++ functions/testbd.js
@@ -1,6 +1,23 @@
 export async function onRequest(event) {
-const ps = event.env.BEBROID.prepare('SELECT * from qoca');
-const data = await ps.all();
-const jsonData = JSON.stringify(data);
-return new Response(jsonData);
+  const ps = event.env.BEBROID.prepare('SELECT * from qoca');
+  const data = await ps.all();
+
+  // Формируем строку с парами "id, string"
+  let resultString = '';
+
+  // Проходим по каждой записи данных
+  data.forEach((row, index) => {
+    // Добавляем "id, string" в строку
+    resultString += `${row.id}, ${row.string}`;
+
+    // Добавляем запятую, если это не последняя запись
+    if (index < data.length - 1) {
+      resultString += ', ';
+    }
+  });
+
+  // Возвращаем строку как ответ
+  return new Response(resultString, {
+    headers: { 'Content-Type': 'text/plain' },
+  });
 }

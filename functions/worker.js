d20575651475b66472b32ce77eda89bbbf17c4ca
WarGack <53942687+WarGack@users.noreply.github.com>
2024-04-29 02:14:20 +0300

    Update worker.js

--- functions/worker.js
+++ functions/worker.js
@@ -1,6 +1,24 @@
 export async function onRequest(event) {
-const ps = event.env.BEBROID.prepare('INSERT INTO qoca (string) VALUES ("bebrochka_pisechka"');
-const data = await ps.all();
-const jsonData = JSON.stringify(data);
-return new Response(jsonData);
+    if (event.request.method !== 'POST') {
+        return new Response('Method Not Allowed', { status: 405 });
+    }
+
+    try {
+        const requestBody = await event.request.text();
+        const formData = new URLSearchParams(requestBody);
+
+        const string = formData.get('string'); // Получаем значение 'string' из формы
+
+        if (!string) {
+            throw new Error('String value is required');
+        }
+
+        const ps = event.env.BEBROID.prepare('INSERT INTO qoca (string) VALUES (?)');
+        const result = await ps.run(string);
+
+        return new Response('Data added successfully!', { status: 200 });
+    } catch (error) {
+        console.error('Error adding data to database:', error);
+        return new Response('Failed to add data to database.', { status: 500 });
+    }
 }

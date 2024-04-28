556cc61306f1c7e5db80bec194cd3437c9d29dfc
WarGack <53942687+WarGack@users.noreply.github.com>
2024-04-28 23:47:36 +0300

    Update testbd.js

--- functions/testbd.js
+++ functions/testbd.js
@@ -3,27 +3,10 @@ export async function onRequest(event) {
   
   try {
     const data = await ps.all();
+    const jsonData = JSON.stringify(data);
     
-    // Формируем HTML-код для таблицы
-    let tableHtml = '<table><thead><tr><th>ID</th><th>String</th></tr></thead><tbody>';
-    
-    // Проверяем, что данные существуют и являются массивом
-    if (Array.isArray(data)) {
-      // Проходим по каждой записи данных и добавляем строки таблицы
-      data.forEach(row => {
-        tableHtml += `<tr><td>${row.id}</td><td>${row.string}</td></tr>`;
-      });
-    }
-    
-    tableHtml += '</tbody></table>';
-    console.log(data); // Вывод данных в консоль для проверки
-    // Возвращаем HTML-страницу с таблицей
-    return new Response(tableHtml, {
-      headers: { 'Content-Type': 'text/html' },
-    });
-  } catch (error) {
-    return new Response('Error fetching data from database', {
-      status: 500,
+    return new Response(jsonData, {
+      headers: { 'Content-Type': 'application/json' },
     });
   }
-}
+  }

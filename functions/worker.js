32c9cd435abb5bc31495ef8dbfcc97235a67d845
WarGack <53942687+WarGack@users.noreply.github.com>
2024-04-29 02:10:26 +0300

    Update worker.js

--- functions/worker.js
+++ functions/worker.js
@@ -1,5 +1,5 @@
 export async function onRequest(event) {
-const ps = event.env.BEBROID.prepare('SELECT * from qoca');
+const ps = event.env.BEBROID.prepare('INSERT INTO qoca (string) VALUES ("bebrochka_pisechka"');
 const data = await ps.all();
 const jsonData = JSON.stringify(data);
 return new Response(jsonData);

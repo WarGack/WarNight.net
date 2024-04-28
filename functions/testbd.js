62ba670a57a9bcfbf0b553d00196f3580bb89ea2
WarGack <53942687+WarGack@users.noreply.github.com>
2024-04-28 23:48:53 +0300

    Update testbd.js

--- functions/testbd.js
+++ functions/testbd.js
@@ -1,12 +1,10 @@
 export async function onRequest(event) {
   const ps = event.env.BEBROID.prepare('SELECT * from qoca');
   
-  try {
+
     const data = await ps.all();
     const jsonData = JSON.stringify(data);
     
     return new Response(jsonData, {
       headers: { 'Content-Type': 'application/json' },
     });
-  }
-  }

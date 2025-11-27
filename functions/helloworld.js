00a15d41f64f4b9c9db0dcbe2a55baa319a1ae0d
WarGack <53942687+WarGack@users.noreply.github.com>
2025-11-27 17:25:56 +0300

    Delete functions directory

--- functions/helloworld.js
+++
@@ -1,7 +0,0 @@
-export function onRequest(context) {
-  const message = "<strong>Hello, world!</strong>";
-
-  return new Response(message, {
-    headers: { 'Content-Type': 'text/html' },
-  });
-}

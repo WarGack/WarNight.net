ecf8cda2ec4deec7116c56153d78cb4763eb8d5c
WarGack <53942687+WarGack@users.noreply.github.com>
2025-12-28 22:14:50 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,32 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith("/api/")) {
-      // TODO: Add your custom /api/* logic here.
-      return new Response("Ok");
-    }
-    // Otherwise, serve the static assets.
-    // Without this, the Worker will error and no assets will be served.
-    return env.ASSETS.fetch(request);
-  },
-};
+  async fetch(req){
+    if(req.method==="OPTIONS")
+      return new Response("",{headers:{
+        "Access-Control-Allow-Origin":"*",
+        "Access-Control-Allow-Headers":"Content-Type"
+      }})
+
+    if(new URL(req.url).pathname!=="/create-invoice")
+      return new Response("404")
+
+    const { amount, email } = await req.json()
+
+    const r = await fetch("https://api.nowpayments.io/v1/invoice",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+        "x-api-key":"REBP4MH-7674DWE-PCDS26J-WEDFK0F"
+      },
+      body: JSON.stringify({
+        price_amount: amount,
+        price_currency: "usd",
+        order_description: email
+      })
+    })
+
+    return new Response(
+      JSON.stringify({ invoice_url:(await r.json()).invoice_url }),
+      { headers:{ "Access-Control-Allow-Origin":"*" } }
+    )
+  }
+}

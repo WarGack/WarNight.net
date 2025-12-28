fc6f94ed30ac38daa9adee8d62be8c2c239535d3
WarGack <53942687+WarGack@users.noreply.github.com>
2025-12-28 22:27:16 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,32 +1,88 @@
 export default {
   async fetch(req){
-    if(req.method==="OPTIONS")
-      return new Response("",{headers:{
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Headers":"Content-Type"
-      }})
+    const u=new URL(req.url)
 
-    if(new URL(req.url).pathname!=="/create-invoice")
-      return new Response("404")
+    if(u.pathname==="/api/create-invoice"){
+      const {nick}=await req.json()
 
-    const { amount, email } = await req.json()
-
-    const r = await fetch("https://api.nowpayments.io/v1/invoice",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json",
-        "x-api-key":"REBP4MH-7674DWE-PCDS26J-WEDFK0F"
-      },
-      body: JSON.stringify({
-        price_amount: amount,
-        price_currency: "usd",
-        order_description: email
+      const r=await fetch("https://api.nowpayments.io/v1/invoice",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json",
+          "x-api-key":"REBP4MH-7674DWE-PCDS26J-WEDFK0F"
+        },
+        body:JSON.stringify({
+          price_amount:4.99,
+          price_currency:"usd",
+          order_description:nick||""
+        })
       })
-    })
 
-    return new Response(
-      JSON.stringify({ invoice_url:(await r.json()).invoice_url }),
-      { headers:{ "Access-Control-Allow-Origin":"*" } }
-    )
+      return new Response(JSON.stringify({invoice_url:(await r.json()).invoice_url}),{
+        headers:{"Content-Type":"application/json"}
+      })
+    }
+
+    if(u.pathname==="/api/fk"){
+      const {nick}=await req.json()
+
+      const m="32189"
+      const oa="4.99"
+      const currency="USD"
+      const o="donate-"+Date.now()
+      const secret1="$)c$kX/wTHE,Ex("
+
+      const s=md5(`${m}:${oa}:${secret1}:${currency}:${o}`)
+      const url=`https://pay.fk.money/?m=${enc(m)}&oa=${enc(oa)}&currency=${enc(currency)}&o=${enc(o)}&us_nick=${enc(nick||"")}&s=${s}`
+
+      return new Response(JSON.stringify({url}),{
+        headers:{"Content-Type":"application/json"}
+      })
+    }
+
+    return new Response("404")
   }
 }
+
+const enc=encodeURIComponent
+
+function md5(s){return rhex(binl_md5(str2binl(s),s.length*8))}
+function rhex(n){let s="",j;for(j=0;j<4;j++)s+=hex_chr[(n>>>(j*8+4))&15]+hex_chr[(n>>>(j*8))&15];return s}
+function str2binl(s){let b=[],i;for(i=0;i<s.length*8;i+=8)b[i>>5]|=(s.charCodeAt(i/8)&255)<<(i%32);return b}
+function binl_md5(x,len){
+  x[len>>5]|=0x80<<((len)%32);x[(((len+64)>>>9)<<4)+14]=len
+  let a=1732584193,b=-271733879,c=-1732584194,d=271733878,i
+  for(i=0;i<x.length;i+=16){
+    let oa=a,ob=b,oc=c,od=d
+    a=ff(a,b,c,d,x[i+0],7,-680876936);d=ff(d,a,b,c,x[i+1],12,-389564586);c=ff(c,d,a,b,x[i+2],17,606105819);b=ff(b,c,d,a,x[i+3],22,-1044525330)
+    a=ff(a,b,c,d,x[i+4],7,-176418897);d=ff(d,a,b,c,x[i+5],12,1200080426);c=ff(c,d,a,b,x[i+6],17,-1473231341);b=ff(b,c,d,a,x[i+7],22,-45705983)
+    a=ff(a,b,c,d,x[i+8],7,1770035416);d=ff(d,a,b,c,x[i+9],12,-1958414417);c=ff(c,d,a,b,x[i+10],17,-42063);b=ff(b,c,d,a,x[i+11],22,-1990404162)
+    a=ff(a,b,c,d,x[i+12],7,1804603682);d=ff(d,a,b,c,x[i+13],12,-40341101);c=ff(c,d,a,b,x[i+14],17,-1502002290);b=ff(b,c,d,a,x[i+15],22,1236535329)
+
+    a=gg(a,b,c,d,x[i+1],5,-165796510);d=gg(d,a,b,c,x[i+6],9,-1069501632);c=gg(c,d,a,b,x[i+11],14,643717713);b=gg(b,c,d,a,x[i+0],20,-373897302)
+    a=gg(a,b,c,d,x[i+5],5,-701558691);d=gg(d,a,b,c,x[i+10],9,38016083);c=gg(c,d,a,b,x[i+15],14,-660478335);b=gg(b,c,d,a,x[i+4],20,-405537848)
+    a=gg(a,b,c,d,x[i+9],5,568446438);d=gg(d,a,b,c,x[i+14],9,-1019803690);c=gg(c,d,a,b,x[i+3],14,-187363961);b=gg(b,c,d,a,x[i+8],20,1163531501)
+    a=gg(a,b,c,d,x[i+13],5,-1444681467);d=gg(d,a,b,c,x[i+2],9,-51403784);c=gg(c,d,a,b,x[i+7],14,1735328473);b=gg(b,c,d,a,x[i+12],20,-1926607734)
+
+    a=hh(a,b,c,d,x[i+5],4,-378558);d=hh(d,a,b,c,x[i+8],11,-2022574463);c=hh(c,d,a,b,x[i+11],16,1839030562);b=hh(b,c,d,a,x[i+14],23,-35309556)
+    a=hh(a,b,c,d,x[i+1],4,-1530992060);d=hh(d,a,b,c,x[i+4],11,1272893353);c=hh(c,d,a,b,x[i+7],16,-155497632);b=hh(b,c,d,a,x[i+10],23,-1094730640)
+    a=hh(a,b,c,d,x[i+13],4,681279174);d=hh(d,a,b,c,x[i+0],11,-358537222);c=hh(c,d,a,b,x[i+3],16,-722521979);b=hh(b,c,d,a,x[i+6],23,76029189)
+    a=hh(a,b,c,d,x[i+9],4,-640364487);d=hh(d,a,b,c,x[i+12],11,-421815835);c=hh(c,d,a,b,x[i+15],16,530742520);b=hh(b,c,d,a,x[i+2],23,-995338651)
+
+    a=ii(a,b,c,d,x[i+0],6,-198630844);d=ii(d,a,b,c,x[i+7],10,1126891415);c=ii(c,d,a,b,x[i+14],15,-1416354905);b=ii(b,c,d,a,x[i+5],21,-57434055)
+    a=ii(a,b,c,d,x[i+12],6,1700485571);d=ii(d,a,b,c,x[i+3],10,-1894986606);c=ii(c,d,a,b,x[i+10],15,-1051523);b=ii(b,c,d,a,x[i+1],21,-2054922799)
+    a=ii(a,b,c,d,x[i+8],6,1873313359);d=ii(d,a,b,c,x[i+15],10,-30611744);c=ii(c,d,a,b,x[i+6],15,-1560198380);b=ii(b,c,d,a,x[i+13],21,1309151649)
+    a=ii(a,b,c,d,x[i+4],6,-145523070);d=ii(d,a,b,c,x[i+11],10,-1120210379);c=ii(c,d,a,b,x[i+2],15,718787259);b=ii(b,c,d,a,x[i+9],21,-343485551)
+
+    a=add(a,oa);b=add(b,ob);c=add(c,oc);d=add(d,od)
+  }
+  return [a,b,c,d]
+}
+function cmn(q,a,b,x,s,t){return add(rol(add(add(a,q),add(x,t)),s),b)}
+function ff(a,b,c,d,x,s,t){return cmn((b&c)|((~b)&d),a,b,x,s,t)}
+function gg(a,b,c,d,x,s,t){return cmn((b&d)|(c&(~d)),a,b,x,s,t)}
+function hh(a,b,c,d,x,s,t){return cmn(b^c^d,a,b,x,s,t)}
+function ii(a,b,c,d,x,s,t){return cmn(c^(b|(~d)),a,b,x,s,t)}
+function add(x,y){let l=(x&0xFFFF)+(y&0xFFFF),m=(x>>>16)+(y>>>16)+(l>>>16);return (m<<16)|(l&0xFFFF)}
+function rol(n,c){return (n<<c)|(n>>>(32-c))}
+const hex_chr="0123456789abcdef".split("")

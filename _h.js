6776350fa73506aaec1c4c31b41b4e5a4c2cd9ee
WarGack <53942687+WarGack@users.noreply.github.com>
2026-01-04 00:00:10 +0300

    Update _h.js

--- _h.js
+++ _h.js
@@ -1,6 +1,10 @@
+var p = location.pathname
+var d = p.endsWith("/demonlist.html") || p.endsWith("demonlist.html")
+var o = p.endsWith("/donate.html") || p.endsWith("donate.html")
+
 document.write(
   '<div class=_nav>'
-  +'<a href="/demonlist.html">Демонглист</a>'
-  +'<a href="/donate.html">Донат</a>'
+  +'<a href="/demonlist.html"'+(d?' class=_on':'')+'>Демонлист</a>'
+  +'<a href="/donate.html"'+(o?' class=_on':'')+'>Донат</a>'
   +'</div><hr>'
 )

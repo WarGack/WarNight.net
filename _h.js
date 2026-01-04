c0ace14e7521bfdf78afd54ff9d144f4b0c687d9
WarGack <53942687+WarGack@users.noreply.github.com>
2026-01-04 22:20:39 +0300

    Refresh WarNight site interface

--- _h.js
+++ _h.js
@@ -1,10 +1,15 @@
-var p = location.pathname.replace(/\/+$/,"") || "/"
-var d = p=="/demonlist" || p=="/demonlist.html"
-var o = p=="/donate" || p=="/donate.html" || p.startsWith("/shop")
+var p = location.pathname.replace(/\/+$/, "") || "/"
+var isHome = p == "/" || p == "/index" || p == "/index.html"
+var isDemon = p == "/demonlist" || p == "/demonlist.html"
+var isDonate = p == "/donate" || p == "/donate.html" || p.startsWith("/shop")
 
 document.write(
-  '<div class=_nav>'
-  +'<a href="/demonlist"'+(d?' class=_on':'')+'>Демонлист</a>'
-  +'<a href="/donate"'+(o?' class=_on':'')+'>Донат</a>'
-  +'</div><hr>'
+  '<div class="_nav">'
+  + '<div class="_logo">WarNight</div>'
+  + '<div class="_navLinks">'
+  + '<a href="/"' + (isHome ? ' class="_on"' : '') + '>Главная</a>'
+  + '<a href="/demonlist"' + (isDemon ? ' class="_on"' : '') + '>Демонлист</a>'
+  + '<a href="/donate"' + (isDonate ? ' class="_on"' : '') + '>Донат</a>'
+  + '</div>'
+  + '</div><hr>'
 )
